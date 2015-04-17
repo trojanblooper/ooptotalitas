c4ef54cf85dbd4983eaea8edcdfa67612edde171
trojanblooper <trojanblooper@yahoo.com>
2015-04-18 00:07:59 +0700

    Create Posisi.java

---
+++ Posisi.java
@@ -0,0 +1,23 @@
+public class Posisi{
+	private int x;
+	private int y;
+
+	public Posisi(int x,int y){
+		setX(x);
+		setY(y);
+	}
+
+	//getter setter
+	public void setX(int x){
+		this.x=x;
+	}
+	public void setY(int y){
+		this.y=y;
+	}
+	public int getX(){
+		return x;
+	}
+	public int getY(){
+		return y;
+	}
+}
